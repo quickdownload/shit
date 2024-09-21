76b6b82a7ea4b9301970c62027981d40dc9862ad
quickdownload <micnikprelaj@gmail.com>
2024-09-22 00:31:55 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,16 +1,17 @@
 local add = {}
 local functions = {
-    io = {},
-    io.write = [[getgenv().print = function(msg)
+    io = {
+write = [[getgenv().print = function(msg)
         print("Secment : " .. tostring(msg) .. "\n")
     end]]
 }
+}
 
-function add:func(path)
+function add:function(path)
     local name, child = path:match("([^%.]+)%.([^%.]+)")
     
     if name and child then
-        local new = functions[name .. "." .. child]
+        local new = functions[name][child]
         loadstring(new)()
     else
         local new = functions[path]
